3c85f369f3fe2df1932c84e9fed57b7553ab1a06
arka1002 <arka.mcu700@gmail.com>
2023-01-17 21:18:48 +0530

    made root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,29 @@
+import HomeIcon from '@mui/icons-material/Home';
+import { Flex, Divider } from "@aws-amplify/ui-react";
+import PersonIcon from '@mui/icons-material/Person';
+import { Outlet } from "react-router-dom";
+import { Amplify } from 'aws-amplify';
+import awsExports from '../aws-exports';
+Amplify.configure(awsExports);
+
+
+
 export default function Root() {
+
+    console.log(`Hi mom!!`);
     return (
         <>
-            <div className="italic">Home</div>
+            <div className="flex flex-row h-24 items-center justify-evenly">
+
+                <h1 className='text-xl italic font-bold underline underline-offset-2'>Home <HomeIcon /> </h1>
+
+                <h1 className='text-xl italic font-bold underline underline-offset-2'>My Profile <PersonIcon /> </h1>
+            </div>
+            <Flex direction="column">
+                <Divider
+                    orientation="horizontal" />
+            </Flex>
+            <div id="detail"><Outlet /></div>
         </>
     );
 };
