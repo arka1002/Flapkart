2e57c4bba4eee23229300b6b5ec56259969db1a6
arka1002 <arka.mcu700@gmail.com>
2023-01-18 19:34:40 +0530

    added quantity

--- src/routes/product.jsx
+++ src/routes/product.jsx
@@ -1,8 +1,9 @@
 import { Amplify } from "aws-amplify";
 import awsExports from '../aws-exports';
 import { useLoaderData } from "react-router-dom";
-
-
+import { Button } from "@aws-amplify/ui-react";
+import { useState } from "react";
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(awsExports);
 
@@ -10,12 +11,25 @@ Amplify.configure(awsExports);
 
 export default function Product() {
     const product = useLoaderData();
+    const [count, setCount] = useState(0);
 
+    function increaseCount() {
+        if (count >= 0 && count < 10) {
+            setCount(count + 1)
+        }
+    }
+
+    function decreaseCount() {
+        if (count >= 1) {
+            setCount(count - 1)
+        }
+    }
 
     return (
         <div key={product.id}>
             <h1>{product.name}</h1>
             <p>{product.description}</p>
+            <Button onClick={decreaseCount}>-</Button><p>{count}</p><Button onClick={increaseCount}>+</Button>
         </div>
     );
 };
