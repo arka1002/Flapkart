fb89960f09c56ba4ea348bbb232078639b5417cb
arka1002 <arka.mcu700@gmail.com>
2023-01-19 21:55:56 +0530

    done filtering in react

--- src/routes/headphones.jsx
+++ src/routes/headphones.jsx
@@ -7,23 +7,44 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import { useState } from "react";
 
 
 export default function Headphones() {
     //use the loader data
     const headphoneList = useLoaderData();
-    const map1 = headphoneList.map(x => x.price).sort((a, b) => a - b);
-    console.log(headphoneList);
-    console.log(map1);
+    const [state, setState] = useState(headphoneList);
+
+
+    function lessthan1000() {
+        const map1000 = headphoneList.filter(product => product.price < 1000);
+        setState(map1000);
+    }
+
+    function lessthan5000() {
+        const map5000 = headphoneList.filter(product => product.price < 5000 && product.price > 1000);
+        setState(map5000);
+    }
+
+    function lessthan10000() {
+        const map10000 = headphoneList.filter(product => product.price < 10000 && product.price > 5000);
+        setState(map10000);
+    }
+
+    function lessthanall() {
+        const mapAll = headphoneList.filter(product => product);
+        setState(mapAll);
+    }
+
 
     return (
         <>
             <p>Price Range</p>
             <ul className="list-none mb-2">
-                <li className="mt-2 ml-2"><Button>All</Button></li>
-                <li className="mt-2 ml-2"><Button>₹0 - ₹1,000</Button></li>
-                <li className="mt-2 ml-2"><Button>₹1,000 - ₹5,000</Button></li>
-                <li className="mt-2 ml-2"><Button>₹5,000 - ₹10,000</Button></li>
+                <li className="mt-2 ml-2"><Button onClick={lessthanall}>All</Button></li>
+                <li className="mt-2 ml-2"><Button onClick={lessthan1000}>₹0 - ₹1,000</Button></li>
+                <li className="mt-2 ml-2"><Button onClick={lessthan5000}>₹1,000 - ₹5,000</Button></li>
+                <li className="mt-2 ml-2"><Button onClick={lessthan10000}>₹5,000 - ₹10,000</Button></li>
             </ul>
             <Flex direction="column">
                 <Divider
@@ -32,7 +53,7 @@ export default function Headphones() {
 
             <Grid container spacing={2}>
                 {
-                    headphoneList.map((headphone) => (
+                    state.map((headphone) => (
                         <Grid item xs={12} md={3}>
                             <div className='flex justify-center'>
                                 <Card sx={{ maxWidth: 345 }}>
@@ -47,8 +68,9 @@ export default function Headphones() {
                                             {headphone.name}
                                         </Typography>
 
-                                        <Typography variant="body2" color="text.secondary">
+                                        <Typography variant="body2" color="text.secondary" component={'span'} >
                                             {headphone.description}
+                                            <span>Price :- {headphone.price}</span>
                                         </Typography>
                                     </CardContent>
                                     <CardActions>
