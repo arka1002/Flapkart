fa51b3f3b43c807029f658e10c2fc9803929c868
arka1002 <arka.mcu700@gmail.com>
2023-01-21 18:19:55 +0530

    will start the profile page now

--- src/routes/product.jsx
+++ src/routes/product.jsx
@@ -1,16 +1,20 @@
 import { useLoaderData } from "react-router-dom";
 import LikeButtonOnProdPage from "../components/likeButtonOnProdPage";
+import { Button } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+
 export default function Product() {
 
     const theProd = useLoaderData();
-    // console.log(theProd);
 
     return (
         <>
-            <h1>{theProd.name}</h1>
+            <h1 className="text-xl italic font-bold underline underline-offset-2">{theProd.name}</h1>
             <p>{theProd.description}</p>
             <p>{theProd.price}</p>
             <LikeButtonOnProdPage id={theProd.id}/>
+            <p><Button>Buy</Button></p>
+
         </>
     );
 };
