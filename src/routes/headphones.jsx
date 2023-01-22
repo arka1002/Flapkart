5b01ecaa570f49b5f5c051feda24443d8140e620
arka1002 <arka.mcu700@gmail.com>
2023-01-22 16:20:44 +0530

    tailoring changes

--- src/routes/headphones.jsx
+++ src/routes/headphones.jsx
@@ -81,7 +81,7 @@ export default function Headphones() {
                                         </Typography>
                                     </CardContent>
                                     <CardActions>
-                                        <NavLink to={`/buy`}><Button>Buy</Button></NavLink><LikeButton id={headphone.id} isLiked={headphone.isLiked} />
+                                        
                                     </CardActions>
                                 </Card>
                             </div>
@@ -97,46 +97,5 @@ export default function Headphones() {
 
 
 
-function LikeButton({ id }) {
-    const queryClient = useQueryClient();
-
-    const mutationliking = useMutation({
-        mutationFn: async (add) => {
-            await API.graphql({
-                query: updateProduct,
-                variables: { input: { id: id, isLiked: add } }
-            })
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['likes'] })
-        },
-    })
 
 
-    const { isLoading, isError, data, error } = useQuery({
-        queryKey: ['likes', id],
-        queryFn: async () => {
-            const var1 = await API.graphql({
-                query: getProduct,
-                variables: { id: id }
-            });
-            const var2 = var1.data.getProduct;
-            return var2;
-        }
-    })
-
-
-    if (isLoading) {
-        return <span>Loading...</span>
-    }
-
-    if (isError) {
-        return <span>Error: {error.message}</span>
-    }
-
-    return (
-        <>
-            {data.isLiked === "no" ? <Button onClick={() => { mutationliking.mutate("yes") }}><AddShoppingCartIcon /></Button> : <Button onClick={() => { mutationliking.mutate("no") }}><RemoveShoppingCartIcon /></Button>}
-        </>
-    );
-}
