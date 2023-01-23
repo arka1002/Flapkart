ea0fc7087380fdfef7e04a96e3942e6ac1660fd3
arka1002 <arka.mcu700@gmail.com>
2023-01-23 13:55:59 +0530

    final touch

--- src/routes/success.jsx
+++ src/routes/success.jsx
@@ -1,5 +1,17 @@
+import '@aws-amplify/ui-react/styles.css';
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Navigate, useLocation } from "react-router-dom";
+
+
 export default function Success() {
+    const location = useLocation();
+    const { route } = useAuthenticator((context) => [context.route]);
+    if (route !== 'authenticated') {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+
     return <div className="flex flex-row justify-center mt-10">
-        <iframe src="https://giphy.com/embed/W8AgFR0fy0SGY" width="480" height="392" frameBorder="0" class="giphy-embed" allowFullScreen className=""></iframe>
+        <iframe src="https://giphy.com/embed/W8AgFR0fy0SGY" width="480" height="392" frameBorder="0" class="giphy-embed" allowFullScreen title="This is a unique title"></iframe>
     </div>
 };
